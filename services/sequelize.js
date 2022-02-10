1eb7296db14fbad72026d90b1dda8ddc5b037b75
Dmitrii Kiriushkin <kiriushkindmitrii@gmail.com>
2022-02-10 20:15:38 +0300

    Change DB URL

--- services/sequelize.js
+++ services/sequelize.js
@@ -1,5 +1,14 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
+
+sequelize.authenticate();
 
 module.exports = { sequelize, Sequelize };
