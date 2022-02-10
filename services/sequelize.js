28fe57666a8765e95ccd266394aa6fe09f3a9d48
Dmitrii Kiriushkin <kiriushkindmitrii@gmail.com>
2022-02-10 20:12:12 +0300

    Change DB URL

--- services/sequelize.js
+++ services/sequelize.js
@@ -1,10 +1,5 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  { dialect: "postgres", host: process.env.DB_HOST }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 module.exports = { sequelize, Sequelize };
