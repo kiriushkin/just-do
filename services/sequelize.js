825e4d746c43390e1cdba4582b802ece2d117eea
Dmitrii Kiriushkin <kiriushkindmitrii@gmail.com>
2022-02-11 11:02:42 +0300

    add check for null on image update for user

--- services/sequelize.js
+++ services/sequelize.js
@@ -1,14 +1,25 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-});
+let sequelize;
 
-sequelize.authenticate();
+if (process.env.NODE_ENV === "production") {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
+
+  sequelize.authenticate();
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD,
+    { dialect: "postgres", host: process.env.DB_HOST }
+  );
+}
 
 module.exports = { sequelize, Sequelize };
