813043beee1039843cb6a80c7167ec0e3c3cf46e
Dmitrii Kiriushkin <kiriushkindmitrii@gmail.com>
2022-02-11 11:14:34 +0300

    change host

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === "development") {
     );
   });
 } else {
-  server = app.listen("8080");
+  server = app.listen("8080", "127.0.0.1");
 }
 
 const io = require("socket.io")(server, {
